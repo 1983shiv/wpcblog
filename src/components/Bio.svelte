489c4bbc3849a16ec052a467c08c1e679f8703df
shivi <1983.shiv@gmail.com>
2021-08-29 23:46:47 +0530

    revised bio part 2

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -4,11 +4,10 @@
 
 
   
-  <div class="flex flex-wrap w-full author-bio bg-shades-100 mt-8 justify-items-center text-left p-6">
-    <img src={author.entry[0].thumbnailUrl} alt={author.entry[0].displayName} class="w-16 h-16 rounded-3xl mt-4">
-    <p class="text-xs">
-      Hi, <br />
-      {author.entry[0].aboutMe}
+  <div class="inline-flex items-center w-full author-bio bg-shades-100 mt-8 p-6">
+    <img src={author.entry[0].thumbnailUrl} alt={author.entry[0].displayName} class="w-16 h-16 rounded-3xl mt-6 " />
+    <p class="text-xs flex-col pl-4 ">
+      Hi, {author.entry[0].aboutMe}
       <!-- You can follow me on 
       {#each author.entry[0].urls as url}
         <a href={url.value} alt={url.title}> {url.title} </a>
