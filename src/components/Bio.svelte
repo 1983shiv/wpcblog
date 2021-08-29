d3c05d53ca1803d419a497d1ec66b505cb9e8c92
shivi <1983.shiv@gmail.com>
2021-08-29 23:31:31 +0530

    revised the author bio

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -7,13 +7,13 @@
   <div class="flex flex-wrap w-full author-bio bg-shades-100 mt-8 justify-items-center text-left p-6">
     <img src={author.entry[0].thumbnailUrl} alt={author.entry[0].displayName} class="w-16 h-16 rounded-3xl mt-4">
     <p class="text-xs">
-      Hi, I'm <strong>{author.entry[0].displayName}</strong>. <br />
+      Hi, <br />
       {author.entry[0].aboutMe}
-      You can follow me on 
+      <!-- You can follow me on 
       {#each author.entry[0].urls as url}
         <a href={url.value} alt={url.title}> {url.title} </a>
         , 
-      {/each}
+      {/each} -->
     </p>
   </div>
   
